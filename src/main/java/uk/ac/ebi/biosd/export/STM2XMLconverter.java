a124e8dce5bc9e0cfd5872fafd1a6b67a5a98e8c
Mike <gostev@ebi.ac.uk>
2012-11-19 11:35:58 +0000

    Schema updated

--- src/main/java/uk/ac/ebi/biosd/export/STM2XMLconverter.java
+++ src/main/java/uk/ac/ebi/biosd/export/STM2XMLconverter.java
@@ -1,8 +1,13 @@
 package uk.ac.ebi.biosd.export;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
+import java.util.Date;
+import java.util.IllegalFormatException;
 import java.util.Iterator;
+import java.util.TimeZone;
 
 import uk.ac.ebi.fg.biosd.model.expgraph.BioSample;
 import uk.ac.ebi.fg.biosd.model.expgraph.properties.SampleCommentValue;
@@ -26,6 +31,11 @@ import uk.ac.ebi.fg.core_model.xref.ReferenceSource;
 
 public class STM2XMLconverter
 {
+ 
+ public static final String updateDate = "Submission Update Date";
+ public static final String releaseDate = "Submission Release Date";
+ public static final String refLayer = "Reference Layer";
+ 
  public enum Samples
  {
   NONE,
@@ -35,6 +45,7 @@ public class STM2XMLconverter
  
  public static final String nameSpace = "http://www.ebi.ac.uk/biosamples/BioSDExportV1";
 
+ private static DateToXsdDatetimeFormatter dateTimeFmt = new DateToXsdDatetimeFormatter( TimeZone.getTimeZone("GMT") );
  
  private static void exportOntologyEntry( OntologyEntry val,  Appendable out) throws IOException
  {
@@ -426,6 +437,28 @@ public class STM2XMLconverter
   if( msi != null )
   {
    
+   if( msi.getReleaseDate() != null )
+   {
+    out.append("<ReleaseDate>");
+    out.append( dateTimeFmt.format(msi.getReleaseDate() ) );
+    out.append("</ReleaseDate>\n");
+   }
+
+   if( msi.getUpdateDate() != null )
+   {
+    out.append("<UpdateDate>");
+    out.append( dateTimeFmt.format(msi.getUpdateDate() ) );
+    out.append("</UpdateDate>\n");
+   }
+
+   if( msi.getSubmissionDate() != null )
+   {
+    out.append("<SubmissionDate>");
+    out.append( dateTimeFmt.format(msi.getSubmissionDate() ) );
+    out.append("</SubmissionDate>\n");
+   }
+
+   
    if( msi.getContacts() != null )
    {
     for( Contact c : msi.getContacts() )
@@ -592,5 +625,51 @@ public class STM2XMLconverter
   if( ! escaping )
    out.append(s);
  }
+ 
+ static class DateToXsdDatetimeFormatter
+ {
+
+  private final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+
+  public DateToXsdDatetimeFormatter()
+  {
+  }
+
+  public DateToXsdDatetimeFormatter(TimeZone timeZone)
+  {
+   simpleDateFormat.setTimeZone(timeZone);
+  }
+
+  /**
+   * Parse a xml date string in the format produced by this class only. This
+   * method cannot parse all valid xml date string formats - so don't try to use
+   * it as part of a general xml parser
+   */
+  public synchronized Date parse(String xmlDateTime) throws ParseException
+  {
+   if(xmlDateTime.length() != 25)
+   {
+    throw new ParseException("Date not in expected xml datetime format", 0);
+   }
+
+   StringBuilder sb = new StringBuilder(xmlDateTime);
+   sb.deleteCharAt(22);
+   return simpleDateFormat.parse(sb.toString());
+  }
+
+  public synchronized String format(Date xmlDateTime) throws IllegalFormatException
+  {
+   String s = simpleDateFormat.format(xmlDateTime);
+   
+   StringBuilder sb = new StringBuilder(s);
+   sb.insert(22, ':');
+   return sb.toString();
+  }
+
+  public synchronized void setTimeZone(String timezone)
+  {
+   simpleDateFormat.setTimeZone(TimeZone.getTimeZone(timezone));
+  }
+}
 
 }
